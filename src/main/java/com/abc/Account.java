2b698fa12e65015d8612ba43fc08de3067c3f144
weilin666 <weilin888@gmail.com>
2016-08-20 00:02:58 -0400

    Update Account.java
    Fixed overdrafting issue for withdraw method.
    Implemented transfer new feature.
    Implemented new maxi_saving account interest rate feature.
    Implemented daily accrue rate.

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -2,6 +2,7 @@ package com.abc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class Account {
 
@@ -10,28 +11,46 @@ public class Account {
     public static final int MAXI_SAVINGS = 2;
 
     private final int accountType;
+    //wei: should it be private?
     public List<Transaction> transactions;
+    private double totalBalance;
+
 
     public Account(int accountType) {
         this.accountType = accountType;
         this.transactions = new ArrayList<Transaction>();
+        totalBalance = 0;
     }
 
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
         } else {
-            transactions.add(new Transaction(amount));
+            //wei:
+            totalBalance = totalBalance + amount;
+            transactions.add(new Transaction(amount, totalBalance));
         }
     }
 
-public void withdraw(double amount) {
+
+public void withdraw(double amount) throws OverdraftException {
+ 
+    //wei: should check the overall balance to prevent overdraftting
+
     if (amount <= 0) {
         throw new IllegalArgumentException("amount must be greater than zero");
     } else {
-        transactions.add(new Transaction(-amount));
+        totalBalance = totalBalance - amount;
+        if(totalBalance < 0) {
+           System.out.println("Overdrating. Withdraw abort.");
+           throw (new OverdraftException("Overdraft"));
+        }else 
+           transactions.add(new Transaction(-amount, totalBalance));
     }
+
 }
+
+/*  not accurate calculation of interest 
 
     public double interestEarned() {
         double amount = sumTransactions();
@@ -41,9 +60,6 @@ public void withdraw(double amount) {
                     return amount * 0.001;
                 else
                     return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
             case MAXI_SAVINGS:
                 if (amount <= 1000)
                     return amount * 0.02;
@@ -54,6 +70,64 @@ public void withdraw(double amount) {
                 return amount * 0.001;
         }
     }
+*/
+
+  //wei: updated method to use daily interest rate
+    public double interestEarned() {
+
+        double interest = 0;
+        Date now = DateProvider.getInstance().now();
+
+        switch(accountType){
+            case SAVINGS:
+               for( Transaction t: transactions ){
+                  long elapsedDays = elapsedDays(t.transactionDate, now); 
+                  if(t.totalBalance <= 1000){
+                     interest = interest + t.amount * elapsedDays * 0.001/365; 
+                  }else{
+                     interest = interest + t.amount * elapsedDays * 0.002/365; 
+                  } 
+               }
+               return interest;
+            case MAXI_SAVINGS:
+               double dailyRate = 0.05/365;
+               for( Transaction t: transactions){
+                  long elapsedDays = elapsedDays(t.transactionDate, now); 
+                  if((t.amount<0) && (elapsedDays <=10)) {
+                     dailyRate = 0.001/365;
+                     break;
+                  }
+               }
+               for( Transaction t: transactions){
+                  long elapsedDays = elapsedDays(t.transactionDate, now); 
+                  interest = interest + t.amount * elapsedDays * dailyRate;
+               }
+               return interest;
+            default:
+               for( Transaction t: transactions){
+                  long elapsedDays = elapsedDays(t.transactionDate, now); 
+                 interest = interest + t.amount * elapsedDays * 0.001/365;
+               }
+               return interest;
+        }       
+    }
+
+
+    //wei: utility method to return days elapsed between two dates
+    public long elapsedDays(Date beginDate, Date endDate){
+
+        long difference = endDate.getTime() - beginDate.getTime();
+        System.out.println(difference);
+
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+
+        long elapsedDays = difference / daysInMilli;
+        return elapsedDays;
+    }
+
 
     public double sumTransactions() {
        return checkIfTransactionsExist(true);
